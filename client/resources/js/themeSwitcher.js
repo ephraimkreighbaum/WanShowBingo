928636cf3194c344c681364f65a2a2446833e92a
brock <1540901+brocksexton@users.noreply.github.com>
2023-10-08 18:47:16 -0400

    Theme URL Argument, FP Theme, Team Page
    Team Page slightly updated, add themeSwitcher arguments, FP Theme

--- client/resources/js/themeSwitcher.js
+++ client/resources/js/themeSwitcher.js
@@ -1,26 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var themeSelector = document.getElementById('themeSelector');
-    var originalStylesheet = document.getElementById('original-stylesheet');
-    var breadStylesheet = document.getElementById('bread-stylesheet'); 
-    var expoStylesheet = document.getElementById('expo-stylesheet'); 
-    var afterdarkStylesheet = document.getElementById('afterdark-stylesheet'); 
-    var liteStylesheet = document.getElementById('lite-stylesheet'); 
-    var darkStylesheet = document.getElementById('dark-stylesheet'); 
+  var themeSelector = document.getElementById('themeSelector');
+  var originalStylesheet = document.getElementById('original-stylesheet');
+  var breadStylesheet = document.getElementById('bread-stylesheet');
+  var expoStylesheet = document.getElementById('expo-stylesheet');
 
-  themeSelector.addEventListener('change', function () {
-    var selectedTheme = themeSelector.value;
-    if (selectedTheme === 'original') {
+  // Function to set theme based on the value
+  function setTheme(theme) {
+    if (theme === 'original') {
       originalStylesheet.disabled = false;
       expoStylesheet.disabled = true;
       breadStylesheet.disabled = true;
-    } else if (selectedTheme === 'bread') {
+    } else if (theme === 'bread') {
       originalStylesheet.disabled = true;
       expoStylesheet.disabled = true;
       breadStylesheet.disabled = false;
-    } else if (selectedTheme === 'ltx23') {
+    } else if (theme === 'ltx23') {
       originalStylesheet.disabled = true;
       expoStylesheet.disabled = false;
       breadStylesheet.disabled = true;
     }
+  }
+
+  // Get the theme parameter from the URL
+  var urlParams = new URLSearchParams(window.location.search);
+  var themeParam = urlParams.get('theme');
+
+  // If there's a theme parameter in the URL, set the theme
+  if (themeParam) {
+    setTheme(themeParam);
+    themeSelector.value = themeParam;  // Update the theme selector dropdown
+  }
+
+  // Also set the theme when the user changes the selection in the theme selector dropdown
+  themeSelector.addEventListener('change', function () {
+    setTheme(themeSelector.value);
   });
 });
